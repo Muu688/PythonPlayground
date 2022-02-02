e0f6486a787f8dc4a8cfd6a25a66844166b442ca
jimi von Burg <rapiddude@hotmail.com>
2022-02-03 09:56:15 +1100

    update

---
+++ Course/MobileApplication/main.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import imp
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
+
+Builder.load_file('design.kv')
+
+class LoginScreen(Screen): # LoginScreen inherits from Screen
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+class RootWidget(ScreenManager):
+    pass
+
+class SignUpScreen(Screen):
+    def add_user(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[username] = {'username' : username, 'password' : password,
+        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+        # print(users)
+
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+
+        self.manager.current = 'sign_up_success_screen'
+
+class SignUpSuccessScreen(Screen):
+    pass
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
