206eb453e0aa3ac626fbf85faf932fcdfa1d272f
Jimi von Burg <rapiddude@hotmail.com>
2022-02-09 10:53:28 +1100

    mouse2

--- Mouse.py
+++ Mouse.py
@@ -2,12 +2,29 @@ import mouse
 
 import time
 
-mins = 30
+mins = 1
 seconds = mins * 60
+origin = ["600", "300"]
 
 while(seconds != 0):
-    mouse.move("500", "500")
+    mouse.move(origin[0], origin[1])
     time.sleep(1)
-    mouse.move("1000", "1000")
-    mins = mins - 1
+    seconds = seconds - 1
+    print(seconds)
+    mouse.move("600", "500")
+    time.sleep(1)
+    seconds = seconds - 1
+    print(seconds)
+    mouse.move("600", "600")
+    time.sleep(1)
+    seconds = seconds - 1
+    print(seconds)
+    mouse.move("600", "700")
+    time.sleep(1)
+    seconds = seconds - 1
+    print(seconds)
+    mouse.move("600", "800")
+    time.sleep(1)
+    seconds = seconds - 1
+    print(seconds)
 
